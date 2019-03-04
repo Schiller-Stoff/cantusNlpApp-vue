87307c65967640ea326624eca79083e310ad3723
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-04 13:25:33 +0100

    added example corpora as data

--- cantusNlp_vueApp/src/App.vue
+++ cantusNlp_vueApp/src/App.vue
@@ -13,7 +13,11 @@ export default {
   },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      corpora: [
+        {name:"salzburg.sal", voy_corpus:"https://voyant-tools.org/tool/Cirrus/?corpus=shakespeare", voy_lemma:"https://voyant-tools.org/tool/Cirrus/?corpus=austen"},
+        {name:"salzburg.pac",voy_corpus:"https://voyant-tools.org/tool/Cirrus/?corpus=austen", voy_lemma:"https://voyant-tools.org/tool/Cirrus/?corpus=shakespeare"}
+      ]
     }
   }
 }
