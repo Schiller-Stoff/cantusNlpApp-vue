381f49e0408f07f580843094946fbba7e19c5aff
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-04 12:18:29 +0100

    built basic grid for the nlp app

--- cantusNlp_vueApp/src/components/AppGrid.vue
+++ cantusNlp_vueApp/src/components/AppGrid.vue
@@ -1,70 +1,11 @@
 <template>
-
-  <div class="card-columns">
-    <div class="card">
-      <img class="card-img-top" src="..." alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Card title that wraps to a new line</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-    <div class="card p-3">
-      <blockquote class="blockquote mb-0 card-body">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-        <footer class="blockquote-footer">
-          <small class="text-muted">
-            Someone famous in <cite title="Source Title">Source Title</cite>
-          </small>
-        </footer>
-      </blockquote>
-    </div>
-    <div class="card">
-      <img class="card-img-top" src="..." alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-    <div class="card bg-primary text-white text-center p-3">
-      <blockquote class="blockquote mb-0">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
-        <footer class="blockquote-footer">
-          <small>
-            Someone famous in <cite title="Source Title">Source Title</cite>
-          </small>
-        </footer>
-      </blockquote>
-    </div>
-    <div class="card text-center">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-    <div class="card">
-      <img class="card-img" src="..." alt="Card image">
-    </div>
-    <div class="card p-3 text-right">
-      <blockquote class="blockquote mb-0">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-        <footer class="blockquote-footer">
-          <small class="text-muted">
-            Someone famous in <cite title="Source Title">Source Title</cite>
-          </small>
-        </footer>
-      </blockquote>
-    </div>
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col col-2"></div>
+      <div class="col col-8"></div>
+      <div class="col col-1"></div>
     </div>
   </div>
-
 </template>
 
 <script>
@@ -74,5 +15,34 @@
 </script>
 
 <style scoped>
+
+  div.row {
+    padding: 10em;
+  }
+
+  div.col {
+    margin: .5em;
+    border-radius: 1em;
+  }
+
+  div.container-fluid {
+    background-color: #eeeeee;
+    min-height: 1000px;
+  }
+
+  .col-1 {
+    background-color: white;
+  }
+
+  .col-2 {
+    background-color: darkblue;
+  }
+
+  .col-8 {
+    background-color: white;
+    min-height: 1000px;
+  }
+
+
 
 </style>
