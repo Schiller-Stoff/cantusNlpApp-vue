923b4f02c5849d7329201f73cc0fb70e02d048a0
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-04 12:33:52 +0100

    added only bootstrap base card example no functionality etc.

--- cantusNlp_vueApp/src/components/VoyantCard.vue
+++ cantusNlp_vueApp/src/components/VoyantCard.vue
@@ -1,5 +1,14 @@
 <template>
-
+  <div class="card text-center p-3">
+    <blockquote class="blockquote mb-0">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
+      <footer class="blockquote-footer">
+        <small>
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
 </template>
 
 <script>
