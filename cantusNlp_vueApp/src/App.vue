f6c8c5f9134a82b9c21a8b6bc31db4d140c3af7e
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-04 12:24:16 +0100

    imported app grid -> added as component

--- cantusNlp_vueApp/src/App.vue
+++ cantusNlp_vueApp/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-
+    <nlp-app-grid></nlp-app-grid>
   </div>
 </template>
 
 <script>
-
+import AppGrid from "./components/AppGrid.vue"
 export default {
   name: 'app',
+  components: {
+    nlpAppGrid: AppGrid
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -17,6 +20,6 @@ export default {
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 </style>
