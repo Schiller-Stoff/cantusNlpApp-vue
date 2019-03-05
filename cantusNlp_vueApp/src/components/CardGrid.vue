fedd48f0de6b44801b1a0197396d861bcaf2504d
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-05 07:01:59 +0100

    added default heading

--- cantusNlp_vueApp/src/components/CardGrid.vue
+++ cantusNlp_vueApp/src/components/CardGrid.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="card-deck">
-    <nlp-app-voyant-card :corpora="corpora" v-for="corpus in cardsToCreate" :linkedCorpus="corpus">
-      <h5>{{corpus.name}}</h5>
-    </nlp-app-voyant-card>
+    <nlp-app-voyant-card :corpora="corpora" v-for="corpus in cardsToCreate" :linkedCorpus="corpus"></nlp-app-voyant-card>
+    <h1 class="container float-left" v-if="cardsToCreate.length===0">Voyant Ansicht hinzufügen</h1>
   </div>
 </template>
 
@@ -30,8 +29,9 @@
 
 <style scoped>
 
-  div {
-    /*background-color: darkred;*/
+  h1 {
+    margin-top:4em;
+    color: lightgrey;
   }
 
 </style>
