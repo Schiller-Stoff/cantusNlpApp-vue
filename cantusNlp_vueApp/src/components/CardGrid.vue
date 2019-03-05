72025502fe4aec9b7350071412974372937f2e45
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-05 17:21:51 +0100

    added some default text content into teh card grid

--- cantusNlp_vueApp/src/components/CardGrid.vue
+++ cantusNlp_vueApp/src/components/CardGrid.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="card-deck">
     <nlp-app-voyant-card :corpora="corpora" v-for="corpus in cardsToCreate" :linkedCorpus="corpus"></nlp-app-voyant-card>
-    <h1 class="container float-left" v-if="cardsToCreate.length===0">Voyant Ansicht hinzufügen</h1>
-  </div>
+    <div class="container" v-if="cardsToCreate.length===0">
+      <h1 class="container" >Wilkommen zur Cantus-Voyant App</h1>
+      <p>
+        Das Cantus Voyant tool dient dazu den Vergleich verschiedener Ansichten zu erleichtern.
+        Die Ansichten selbst stammen aus den VoyantTools und können mit Hilfe einfacer Werkzeuge
+        auf dieser Seite miteinander verglichen werden.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+      <h3>Eine Kurze Anleitung</h3>
+      <ul>
+        <li>Via Click auf das dropdown links oben kann eine Voyant Karte in diese Ansicht eingefügt werden. </li>
+        <li>Auf der linken Seite befinden sich Werkzeuge um Operationen auf alle Karten anwenden zu können.</li>
+        <li>Zur Rechten: Werkzeuge und Beschreibungen zur Erklräung der einzelnen Ansichten der Voyant Tools</li>
+      </ul>
+      <h3>Kontakt und Hilfe</h3>
+    </div>
+    </div>
 </template>
 
 <script>
@@ -30,8 +52,39 @@
 <style scoped>
 
   h1 {
-    margin-top:4em;
+    margin-bottom: .5em;
     color: lightgrey;
+    border-bottom: .15em solid #eeeeee;
+    border-bottom-right-radius: .75em;
+    max-width: 50em;
   }
+
+  h3 {
+    margin-top: 1.5em;
+    color: lightgrey;
+    border-bottom-right-radius: 1em;
+    border-bottom: .2em solid #eeeeee;
+  }
+
+  div.container {
+    margin-top: 4em;
+    padding: 1em;
+    margin-left: 8em;
+    /*border: .1em solid lightgrey;*/
+    /*border-radius: 1em;*/
+    /*box-shadow: .1em .5em 1em .01em grey;*/
+    max-width: 62%;
+  }
+
+  .container li {
+    color: grey;
+  }
+
+  .container p,li {
+    max-width: 62%;
+    text-align: justify;
+  }
+
+
 
 </style>
