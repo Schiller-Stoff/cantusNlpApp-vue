8be29ff54dd25c51d534289cda2bd010ccdeaff2
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-06 19:11:31 +0100

    added icons and message notifications

--- cantusNlp_vueApp/src/components/CardView.vue
+++ cantusNlp_vueApp/src/components/CardView.vue
@@ -1,13 +1,55 @@
 <template>
-
+  <div id="cardView_container">
+    <div>
+      <div @click="notify('Intro Ansichtsleiste')" data-balloon="Intro Ansichtsleiste" data-balloon-pos="up"><i class="fas fa-info-circle fa-2x"></i></div>
+    </div>
+    <hr>
+    <div>
+      <div @click="notify('Ansichten vergrößert')" data-balloon="Ansichten vergrößern" data-balloon-pos="up"><i class="fas fa-th-large fa-2x"></i></div>
+    </div>
+    <div>
+      <div @click="notify('Ansichten verkleinert')" data-balloon="Ansichten verkleinern" data-balloon-pos="up"><i class="fas fa-th fa-2x"></i></div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "CardView"
+  export default {
+    name: "CardView",
+    methods:{
+      notify(title,msg){
+        this.$notify({
+          group: 'foo',
+          title: title,
+          text: msg
+        });
+      }
     }
+  }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+  #cardView_container {
+    margin-top: 35vh;
+  }
+
+  div {
+    margin-top: 2em;
+  }
+
+  i {
+    display: block;
+    margin-top: .5em;
+    margin-left: auto;
+    color:white;
+  }
+
+  hr {
+    color: white;
+    border: .05em white solid;
+    border-radius: 10em;
+    width: 3em;
+  }
 
 </style>
