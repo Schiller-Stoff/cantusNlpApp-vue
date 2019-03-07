bddc33ff47ed1c96b318b405610b6d3e46acf4bd
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-07 14:07:32 +0100

    now you could define the position of the border // and is optional

--- cantusNlp_vueApp/src/mixins/iconMethodsMixin.js
+++ cantusNlp_vueApp/src/mixins/iconMethodsMixin.js
@@ -7,13 +7,31 @@ export const iconMethodsMixin = {
     }
   },
   methods: {
-    markActive(evt){
+    markActive(evt, borderPos='none'){
       let targetElem = evt.target;
       let curCss = evt.target.style;
       if(targetElem!==this.curActive.activeElem){
         targetElem.style.transform = "rotate(180deg)";
         targetElem.style.color = "orange"
-        targetElem.style.borderRight = ".15em solid orange";
+        //assign the right border according to given param
+        switch (borderPos.toLowerCase()){
+          case "left":
+            targetElem.style.borderLeft = ".15em solid orange";
+            break;
+          case "right":
+            targetElem.style.borderRight = ".15em solid orange";
+            break;
+          case "top":
+            targetElem.style.borderTop = ".15em solid orange";
+            break;
+          case "bottom":
+            targetElem.style.borderBottom = ".15em solid orange";
+            break;
+          default:
+            break;
+        }
+
+        //reset style (as soon as an element was assigned)
         if(this.curActive.activeElem!==undefined){
           this.curActive.activeElem.style = this.curActive.defaultCss;
         }
