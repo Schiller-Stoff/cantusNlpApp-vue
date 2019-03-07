6abe2c306e894a0decb5513ed6bf901ff1b5e8c4
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-07 12:29:18 +0100

    added import for scss variables and used them

--- cantusNlp_vueApp/src/components/AppGrid.vue
+++ cantusNlp_vueApp/src/components/AppGrid.vue
@@ -113,7 +113,8 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import "../scss/globalVariables";
 
   div.col {
     margin:0;
@@ -121,14 +122,14 @@
   }
 
   div.container-fluid {
-    background-color: #eeeeee;
+    background-color: $blankColor;
     min-height: 1000px;
     padding-right: 0;
     margin-right: 0;
   }
 
   .col-md-1 {
-    background-color: darkblue;
+    background-color: $primaryColor;
     min-height: 400px;
   }
 
@@ -138,7 +139,7 @@
 
   .container-fluid .row:first-child {
     min-height: 80px;
-    background-color: darkblue;
+    background-color: $primaryColor;
   }
 
   /*Styling for vue tours*/
@@ -146,14 +147,14 @@
   .v-step {
     z-index: 9999;
     text-align: justify;
-    background-color: white;    /*https://color.adobe.com/de/create/color-wheel/?base=2&rule=Compound&selected=3&name=Mein%20Color-Thema&mode=rgb&rgbvalues=0,0,0.5450980392156862,0.24,0.24000000000008187,0.6,0,0.526315789473756,1,1,0.6145833333331439,0.25,0.8,0.32499999999987267,0.07999999999999999&swatchOrder=0,1,2,3,4*/
+    background-color: $blankColor;    /*https://color.adobe.com/de/create/color-wheel/?base=2&rule=Compound&selected=3&name=Mein%20Color-Thema&mode=rgb&rgbvalues=0,0,0.5450980392156862,0.24,0.24000000000008187,0.6,0,0.526315789473756,1,1,0.6145833333331439,0.25,0.8,0.32499999999987267,0.07999999999999999&swatchOrder=0,1,2,3,4*/
     color: black;
     /*min-width: 30em;*/
   }
 
   .v-step button:hover {
     /*background-color: white;*/
-    border: .1em dashed #3D3D99;
+    border: .1em dashed $secondaryColor;
   }
 
   .v-step button {
