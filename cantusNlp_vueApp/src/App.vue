eb0da74161dbb1eda05bd073b95b056ac28d7eeb
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-18 12:16:01 +0100

    added Passau.sve with stable urls

--- cantusNlp_vueApp/src/App.vue
+++ cantusNlp_vueApp/src/App.vue
@@ -44,7 +44,8 @@ export default {
          {name:"Passau_knb", voy_corpus:"https://voyant-tools.org/?corpus=30212f907cd263eac28d07535195c433&input=https://gams.uni-graz.at/archive/objects/o:cantus.passau.knb/datastreams/CORPUS_PLAIN/content&view=Cirrus",voy_lemma:"https://voyant-tools.org/?corpus=f4371588282c87bbf80cbc2ae7df8196&input=https://gams.uni-graz.at/archive/objects/o:cantus.passau.knb/datastreams/CORPUS_LEMMAT/content&view=Cirrus"},
 
          {name:"Passau_sp", voy_corpus:"https://voyant-tools.org/?corpus=bbfa77ef776c60675c400465b3ee673c&input=https://gams.uni-graz.at/archive/objects/o:cantus.passau.sp/datastreams/CORPUS_PLAIN/content&view=Cirrus",voy_lemma:"https://voyant-tools.org/?corpus=02345f59e456cc49a98a9ef721b6e429&input=https://gams.uni-graz.at/archive/objects/o:cantus.passau.sp/datastreams/CORPUS_LEMMAT/content&view=Cirrus"},
-        // {name:"Passau_sve", voy_corpus:"",voy_lemma:""},
+
+         {name:"Passau_sve", voy_corpus:"https://voyant-tools.org/?corpus=3e08cd69fc449c31f897d4292b176130&input=https://gams.uni-graz.at/archive/objects/o:cantus.passau.sve/datastreams/CORPUS_PLAIN/content&view=Cirrus",voy_lemma:"https://voyant-tools.org/?corpus=bf3c4e9f28b863ee3e3a4583163808ad&input=https://gams.uni-graz.at/archive/objects/o:cantus.passau.sve/datastreams/CORPUS_LEMMAT/content&view=Cirrus"},
 
 
         // {name:"Klosterneuburg_knb2", voy_corpus:"https://voyant-tools.org/?input=http://gams.uni-graz.at/archive/objects/o:cantus.brixen/datastreams/TEI_SOURCE/content&view=Cirrus",voy_lemma:"https://voyant-tools.org/?input=http://gams.uni-graz.at/archive/objects/o:cantus.salzburg.sal/datastreams/TEI_SOURCE/content&view=Cirrus"},
