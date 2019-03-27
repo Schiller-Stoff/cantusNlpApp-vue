663d8de8d19fbb691e3e3c33bae2e3e8f9178936
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-27 13:26:59 +0100

    updated content

--- cantusNlp_vueApp/src/components/CardGrid.vue
+++ cantusNlp_vueApp/src/components/CardGrid.vue
@@ -25,7 +25,15 @@
         <p>
           Das Cantus Vorschau-Voyant Werkzeug dient dazu eine schnelle Vorschau verschiedener Voyant Ansichten zu ermöglichen.
           Alle Ansichten stammen direkt von den Voyant-Tools.
+          Hier können erste Eindrücke für den quantitativen Vergleich verschiedener Libri Ordinarii gewonnnen und je nach Bedarf Forschungsdatenmaterial im JSON-Format abgerufen werden.
+          Es handelt sich hierbei um eine verkürzte Vorschau zusätzlich zur eigentlichen Analyse direkt auf der Voyant-Tools Website.
+          Im Felde links neben dem "LO-hinzufügen" Knopf befindet sich ein Auswahlmenü mit dem eine Voyant-Vorschau an Ort und Stelle hinzugefügt werden kann. Einzelne Info-Knöpfe
+          an den drei Seiten des Containers geben weitere Auskünfte über die Verwendung des Vorschauwerkzeuges.
         </p>
+        <hr>
+        <h3>NLP / Korpusanalyse</h3>
+        <p>Hier finden Sie eine Darstellung der NLP-Analyse über alle Libri Ordinarii hinweg, einerseits als Orginaltext und andererseits in lemmatisierter Form. Lemmatisierung, Tokenisierung etc. wurden mit Hilfe des "Classical
+          Language Toolkits" in Python realisiert.</p>
         <hr>
         <h3>Der Lemmatisierte Text</h3>
         <p>
@@ -37,15 +45,17 @@
           über die "Lesetext" - Ansicht angesteuert werden.
         </p>
         <hr>
+
+      </div>
+      <div class="col-lg-6">
+
         <h3>Eine Kurze Anleitung</h3>
         <ul>
           <li>Via Click auf das Navigationsmenü links oben kann eine "Voyant Karte" eingefügt werden. </li>
           <li>Auf der linken Seite befinden sich Werkzeuge um Operationen auf alle Karten anwenden zu können.</li>
           <li>Zur Rechten: Veränderung der Größe der einzelnen Karten, sowie links zum originalen und lemmatisierten
-          Korpus.</li>
+            Korpus.</li>
         </ul>
-      </div>
-      <div class="col-lg-6">
       </div>
 
     </div>
