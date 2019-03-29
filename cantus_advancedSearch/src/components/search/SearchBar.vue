0c751f6ad64d74bd29280a23222b0a2dc20365a3
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-29 15:06:23 +0100

    first design of advanced search page

---
+++ cantus_advancedSearch/src/components/search/SearchBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container-fluid">
+    <div>
+      <div><i class="fas fa-volleyball-ball"></i></div>
+      <p>Feste</p>
+    </div>
+    <hr>
+
+    <form class="inline">
+      <label for="inlineFormCustomSelectPref">Resp</label>
+      <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+        <option selected>Choose...</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search"
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../scss/globalVariables/globalVariables";
+
+  .container-fluid {
+    margin-top: 5em;
+  }
+
+  i {
+    color: white;
+    font-size: 1.5em;
+    margin-top: 1em;
+  }
+
+  p {
+    color: $primaryColor;
+  }
+
+  hr {
+    background-color: $primaryColor
+  }
+
+  label {
+    color: $primaryColor;
+  }
+
+  input {
+    background-color: $primaryColor;
+  }
+
+</style>
