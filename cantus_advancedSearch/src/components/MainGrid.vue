e7b63745b51eb1ec4eca078d55cd2d93cf0d8146
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-03-29 12:46:06 +0100

    added base design

--- cantus_advancedSearch/src/components/MainGrid.vue
+++ cantus_advancedSearch/src/components/MainGrid.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container-fluid">
-    <div class="row"></div>
+    <div class="row">
+      <div class="col-md-1 btn btn-primary">
+        Erweiterte Suche
+      </div>
+      <div class="col-md-10"></div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +16,14 @@
 </script>
 
 <style scoped>
+
+.col-md-1 {
+  background-color: lightblue;
+  height: 50vh;
+}
+
+.col-md-10 {
+  background-color: lightgreen;
+}
 
 </style>
