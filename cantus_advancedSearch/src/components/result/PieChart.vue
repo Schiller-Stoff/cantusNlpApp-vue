2905da962be97fedfc047337fe0e5d740447584b
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-01 10:34:18 +0200

    declared props for piechart data

--- cantus_advancedSearch/src/components/result/PieChart.vue
+++ cantus_advancedSearch/src/components/result/PieChart.vue
@@ -6,17 +6,27 @@
     import {Pie} from 'vue-chartjs'
     export default {
       extends: Pie,
-      mounted () {
-        // Overwriting base render method with actual data.
-        this.renderChart({
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-          datasets: [
+      props:{
+        labels: {
+          type: Array,
+          default: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        },
+        datasets: {
+          type:Array,
+          default: [
             {
               label: 'GitHub Commits',
               backgroundColor: '#f87979',
               data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
             }
           ]
+        }
+      },
+      mounted () {
+        // Overwriting base render method with actual data.
+        this.renderChart({
+          labels: this.labels,
+          datasets: this.datasets
         })
       }
     }
