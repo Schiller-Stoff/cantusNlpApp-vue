32c948f6f75dd6952ee9914adee8344a136d99c5
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-09 14:17:14 +0200

    added testwise result table

--- cantus_advancedSearch/src/components/result/ResultTable.vue
+++ cantus_advancedSearch/src/components/result/ResultTable.vue
@@ -1,12 +1,30 @@
 <template>
     <div>
-      <h2>This is the result table</h2>
+      <div id="people" class="container-fluid">
+        <v-client-table :data="tableData" :columns="columns" :options="options"></v-client-table>
+      </div>
     </div>
 </template>
 
 <script>
   export default {
-    name: "ResultTable"
+    name: "ResultTable",
+    data(){
+      return {
+        columns: ['id', 'name', 'age'],
+        tableData: [
+          { id: 1, name: "John", age: "20" },
+          { id: 2, name: "Jane", age: "24" },
+          { id: 3, name: "Susan", age: "16" },
+          { id: 4, name: "Chris", age: "55" },
+          { id: 5, name: "Dan", age: "40" }
+        ],
+        options: {
+          // see the options API
+        }
+      }
+    }
+
   }
 </script>
 
--- cantus_advancedSearch/src/main.js
+++ cantus_advancedSearch/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import {ServerTable, ClientTable, Event} from 'vue-tables-2';
 
+Vue.use(ClientTable);
 Vue.use(VueResource);
 
 export const EventBus = new Vue();
