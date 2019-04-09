5607d930f57c7363e3e5c3701adb1b6de97f4f15
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-09 16:08:48 +0200

    added font awesome + text

--- cantus_advancedSearch/src/components/result/ResultDefault.vue
+++ cantus_advancedSearch/src/components/result/ResultDefault.vue
@@ -1,5 +1,14 @@
 <template>
 
+  <div>
+    <h2>Cantus Erweiterte Suche</h2>
+    <hr>
+    <div class="ResultDefault_iconHolder">
+      <i class="fas fa-arrow-circle-left"></i>
+      <p>Bewegen Sie die Maus über die blaue Leiste Links um eine Suche zu starten.</p>
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -8,6 +17,15 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+  .ResultDefault_iconHolder {
+    i {
+      color: #0047bb;
+      font-size: 3em;
+      margin: .75em .75em .5em 0;
+    }
+    i,p {display: inline-block;}
+  }
 
 </style>
