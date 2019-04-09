543b26c3a5aa70077cfaf6bb493d06de361133be
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-09 16:42:06 +0200

    blending in furthers arrows delayed

--- cantus_advancedSearch/src/components/result/ResultDefault.vue
+++ cantus_advancedSearch/src/components/result/ResultDefault.vue
@@ -7,14 +7,47 @@
       <i class="fas fa-arrow-circle-left"></i>
       <p>Bewegen Sie die Maus über die blaue Leiste Links um eine Suche zu starten.</p>
     </div>
-  </div>
 
+    <div v-if="showFullContent">
+      <div class="ResultDefault_iconHolder">
+        <i class="fas fa-arrow-circle-left"></i>
+        <p>Bewegen Sie die Maus über die blaue Leiste Links um eine Suche zu starten.</p>
+      </div>
+      <div class="ResultDefault_iconHolder">
+        <i class="fas fa-arrow-circle-left"></i>
+        <p>Bewegen Sie die Maus über die blaue Leiste Links um eine Suche zu starten.</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "ResultDefault"
+  let timer;
+  import {EventBus} from "../../main";
+  export default {
+    name: "ResultDefault",
+    components: {
+
+    },
+    data(){
+      return {
+        showFullContent:false
+      }
+    },
+    created(){
+      EventBus.$on('searchBarMinified',_=>{
+        if(!this.showFullContent)return clearTimeout(timer);
+        this.showFullContent = false;
+      });
+
+      EventBus.$on('searchBarEnlarge',_=>{
+        if(this.showFullContent)return;
+        timer = setTimeout(_=>{
+          this.showFullContent = true;
+        },500)
+      });
     }
+  }
 </script>
 
 <style scoped lang="scss">
