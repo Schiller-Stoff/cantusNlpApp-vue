f45afab9cd5aa65f522e23cc40ea73f7ffd9bfe6
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-15 10:40:14 +0200

    added error message

--- cantus_advancedSearch/src/components/result/ResultLoadHandler.vue
+++ cantus_advancedSearch/src/components/result/ResultLoadHandler.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="container-fluid text-center">
-    <i class="fas fa-circle-notch fa-spin"></i>
+    <i class="fas fa-circle-notch fa-spin" v-if="!loadFailed"></i>
+    <div v-else>
+      <h5>Suchfehler: Momentan ist leider keine Verbindung zur Datenbank möglich. Bitte versuchen Sie es später erneut.</h5>
+    </div>
   </div>
 </template>
 <script>
   export default {
-    name: "ResultLoadHandler"
+    name: "ResultLoadHandler",
+    props: {
+      loadFailed: {
+        type: Boolean,
+        required: true,
+        default: false
+      },
+    }
   }
 </script>
 
 <style scoped lang="scss">
+  @import "../../scss/globalVariables/globalVariables";
   .container-fluid {
     margin-top: 40vh;
     i {
