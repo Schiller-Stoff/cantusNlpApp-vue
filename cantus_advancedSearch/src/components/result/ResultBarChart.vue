9301545dc1b6682d543ede35a0ef1e50ac8d7fca
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-15 12:22:37 +0200

    added app bar chart testwise

---
+++ cantus_advancedSearch/src/components/result/ResultBarChart.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div>
+    <app-bar-chart></app-bar-chart>
+  </div>
+
+</template>
+
+<script>
+  import BarChart from './charts/BarChart'
+  export default {
+    name: "ResultBarChart",
+    components: {
+      appBarChart: BarChart
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
