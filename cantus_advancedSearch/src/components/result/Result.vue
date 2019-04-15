55f7b09b3273c4ea22f7713d535e11ffb4d02ac4
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-15 12:26:53 +0200

    added app bar chart testwise --- data dependancy still to create!

--- cantus_advancedSearch/src/components/result/Result.vue
+++ cantus_advancedSearch/src/components/result/Result.vue
@@ -1,35 +1,41 @@
 <template>
   <div class="container-fluid">
-      <app-result-default
-        :key="0"
-        v-if="!searchResult && !waitingForSearchResult">
-      </app-result-default>
-      <app-result-preview
-        v-if="showPreview && searchResult && !waitingForSearchResult"
-        :searchParams="searchParams"
-        :prevData="searchResult"
-        :key="1">
-      </app-result-preview>
-      <app-result-table
-        v-if="!showPreview && searchResult && !waitingForSearchResult"
-        :key="2"
-        :tableData="searchResult"
-        :searchParams="searchParams">
-      </app-result-table>
+    <app-result-default
+      :key="0"
+      v-if="!searchResult && !waitingForSearchResult">
+    </app-result-default>
+    <app-result-preview
+      v-if="showPreview && searchResult && !waitingForSearchResult"
+      :searchParams="searchParams"
+      :prevData="searchResult"
+      :key="1">
+    </app-result-preview>
+    <app-result-table
+      v-if="!showPreview && searchResult && !waitingForSearchResult"
+      :key="2"
+      :tableData="searchResult"
+      :searchParams="searchParams">
+    </app-result-table>
 
-      <app-result-card-grid
-        :key="3"
-        :search-history="searchHistory"
-        v-if="!showPreview && searchResult && !waitingForSearchResult"
-      >
-      </app-result-card-grid>
+    <app-result-card-grid
+      :key="3"
+      :search-history="searchHistory"
+      v-if="!showPreview && searchResult && !waitingForSearchResult"
+    >
+    </app-result-card-grid>
 
-      <app-result-load-handler
-        v-if="waitingForSearchResult"
-        :key="4"
-        :loadFailed="loadFailed"
-      >
-      </app-result-load-handler>
+    <app-result-load-handler
+      v-if="waitingForSearchResult"
+      :key="4"
+      :loadFailed="loadFailed"
+    >
+    </app-result-load-handler>
+
+    <app-result-bar-chart
+      v-if="!showPreview && searchResult && !waitingForSearchResult"
+    >
+
+    </app-result-bar-chart>
 
   </div>
 </template>
@@ -43,14 +49,16 @@
   import ResultDefault from './ResultDefault'
   import ResultCardGrid from './ResultCardGrid'
   import ResultLoadHandler from './ResultLoadHandler'
+  import ResultBarChart from './ResultBarChart'
+
   export default {
     name: "Result.vue",
-    data(){
+    data() {
       return {
         searchHistory: [],
-        searchResult:undefined,
-        searchParams:undefined,
-        showPreview:false,
+        searchResult: undefined,
+        searchParams: undefined,
+        showPreview: false,
         waitingForSearchResult: false,
         loadFailed: false
       }
@@ -58,12 +66,13 @@
     components: {
       appResultPreview: ResultPreview,
       appResultTable: ResultTable,
-      appResultDefault:ResultDefault,
-      appResultCardGrid:ResultCardGrid,
-      appResultLoadHandler: ResultLoadHandler
+      appResultDefault: ResultDefault,
+      appResultCardGrid: ResultCardGrid,
+      appResultLoadHandler: ResultLoadHandler,
+      appResultBarChart: ResultBarChart
     },
-    created(){
-      EventBus.$on('resultReceived',data=>{
+    created() {
+      EventBus.$on('resultReceived', data => {
         // if result received clear searchTimer
         clearTimeout(searchTimer)
         this.waitingForSearchResult = false;
@@ -74,36 +83,36 @@
         this.showPreview = true
       });
 
-      EventBus.$on('searchStarted',_=>{
+      EventBus.$on('searchStarted', _ => {
         this.loadFailed = false;
         this.waitingForSearchResult = true
 
         //timer will be cleared insight resultReceived event above.
         let self = this
-        searchTimer = setTimeout(_=>{
+        searchTimer = setTimeout(_ => {
           self.loadFailed = true;
-        },10000);
+        }, 10000);
       })
 
-      EventBus.$on('searchFailed',err=>{
+      EventBus.$on('searchFailed', err => {
         this.loadFailed = true;
         //TODO add error display in component
 
       });
 
-      EventBus.$on('searchBarMinified', _=> {
+      EventBus.$on('searchBarMinified', _ => {
         this.showPreview = false;
 
         //clears delayed blend in via setTimeout
         clearTimeout(curTimer)
       })
 
-      EventBus.$on('searchBarEnlarge', _=> {
-        if(!this.searchResult)return;
-        curTimer = setTimeout(_=>{
-          if(this.showPreview)this.showPreview = false;
+      EventBus.$on('searchBarEnlarge', _ => {
+        if (!this.searchResult) return;
+        curTimer = setTimeout(_ => {
+          if (this.showPreview) this.showPreview = false;
           this.showPreview = true;
-        },500)
+        }, 500)
       })
 
 
