575b40249feeaf6e09e8e93e72b41a5772560c08
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-15 13:02:09 +0200

    changed hardcoded values to required props with default vals

--- cantus_advancedSearch/src/components/result/charts/BarChart.vue
+++ cantus_advancedSearch/src/components/result/charts/BarChart.vue
@@ -5,17 +5,31 @@
 
   export default {
     extends: Bar,
+    props: {
+      labels: {
+        type: Array,
+        required: true,
+        default(){
+          return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        }
+      },
+      datasets: {
+        type:Array,
+        required:true,
+        default(){
+          return [{
+              label: 'GitHub Commits',
+              backgroundColor: 'blue',
+              data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+            }]
+        }
+      }
+    },
     mounted() {
       // Overwriting base render method with actual data.
       this.renderChart({
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        datasets: [
-          {
-            label: 'GitHub Commits',
-            backgroundColor: 'blue',
-            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-          }
-        ]
+        labels: this.labels,
+        datasets: this.datasets
       })
     }
   }
