b6e2d230d450094178fdb4d10c8a1f94f5cf717a
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-16 15:28:37 +0200

    implemented searchParams

--- cantus_advancedSearch/src/store/modules/search.js
+++ cantus_advancedSearch/src/store/modules/search.js
@@ -12,18 +12,32 @@ const state = {
 const mutations = {
   'search_modifyCurSearchQuery'(state,payload){
     state.curSearchQuery = payload
+  },
+
+  'search_setSearchParams'(state,payload){
+    let requiredKeys = ['chosenGenre','chosenLO','chosenTimeFrame']
+    for (let keys in requiredKeys){
+      if(!payload.hasOwnProperty(keys))console.error(`InvalidSearchInput: Could not find required key ${key} in given searchParams object. ${payload}`);
+    }
+    state.searchParams = payload
   }
 }
 
 const actions = {
   'search_modifyCurSearchQueryAction'({commit},payload){
     commit('search_modifyCurSearchQuery',payload)
+  },
+  'search_setSearchParamsAction'({commit},payload){
+    commit('search_setSearchParams',payload)
   }
 }
 
 const getters = {
   'search_getCurSearchQuery'(state){
     return state.curSearchQuery
+  },
+  'search_getSearchParams'(state){
+    return state.searchParams
   }
 
 }
