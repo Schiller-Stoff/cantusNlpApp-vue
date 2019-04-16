08518aea19ca27f0a5446d5a3153c23341e5ef1b
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-16 11:45:51 +0200

    base implementation for interface states

---
+++ cantus_advancedSearch/src/store/modules/interfaceStates.js
@@ -0,0 +1,26 @@
+
+
+const state = {
+  interfaceEnlarged:false
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+
+const getters = {
+
+}
+
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
