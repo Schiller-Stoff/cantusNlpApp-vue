b9bd8f26966f6f3e1023b96865af26dea8696a30
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-17 14:15:38 +0200

    implemented action + mutations + getters for curVizData

--- cantus_advancedSearch/src/store/modules/vizData.js
+++ cantus_advancedSearch/src/store/modules/vizData.js
@@ -1,26 +1,36 @@
 
 
 const state = {
-
+  curVizData: {
+    labels:undefined,
+    datasets: undefined
+  },
+  vizDataStore: []
 
 }
 
 const mutations = {
-
-
+  'viz_setCurVizData'(state,payload){
+    let requiredKeys = ['labels','datasets']
+    for (let key of requiredKeys){
+      if(!payload.hasOwnProperty(key))console.error(`InvalidState: Couldn't find required key ${key} inside curVizData in vuex store. Given object was: ${payload}`)
+    }
+    state.curVizData = payload
+  }
 
 }
 
 const actions = {
-
-
+  'viz_setCurVizDataAction'({commit},payload){
+    commit('viz_setCurVizData',payload)
+  }
 
 }
 
 const getters = {
-
-
-
+  'viz_getCurVizData'(state){
+    return state.curVizData
+  }
 
 }
 
