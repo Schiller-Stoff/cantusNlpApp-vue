ba5a501947d4b1eba3597ee26e1f2b9df3b5a2b1
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-18 16:01:15 +0200

    added testwise linechart

---
+++ cantus_advancedSearch/src/components/result/charts/LineChart.vue
@@ -0,0 +1,24 @@
+
+<script>
+  // CommitChart.js
+  import {Line} from 'vue-chartjs'
+
+  export default {
+    extends: Line,
+    props: {
+      chartData:{
+        type:Object
+      }
+    },
+    watch: {
+      datasets(){
+        this.renderChart(this.chartData)
+      }
+    },
+    mounted() {
+      // Overwriting base render method with actual data.
+      this.renderChart(this.chartData)
+    }
+  }
+</script>
+
