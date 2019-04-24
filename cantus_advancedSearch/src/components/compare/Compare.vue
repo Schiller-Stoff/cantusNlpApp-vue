11aea1982b0278d100c3930fde579433bb2f1efa
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-24 14:55:21 +0200

    included base layout of the compare.vue component

---
+++ cantus_advancedSearch/src/components/compare/Compare.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container-fluid">
+      <h5>I'm the compare viz</h5>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Compare"
+    }
+</script>
+
+<style scoped lang="scss">
+  @import "../../scss/globalVariables/globalVariables";
+
+  .container-fluid {
+    @include lg {
+      margin-top: 1em;
+      min-height: 50vh;
+    }
+    background-color: white;
+  }
+
+</style>
