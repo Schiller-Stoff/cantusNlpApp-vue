32784f0f0b54246cfd8cd2bf8b0f81118073a649
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-29 13:35:58 +0200

    added two components to reflect future layout?

---
+++ cantus_advancedSearch/src/components/result/compare/ResultPreviewCompare.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "ResultPreviewCompare"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ cantus_advancedSearch/src/components/result/compare/ResultPreviewCountall.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "ResultPreviewCuntall"
+    }
+</script>
+
+<style scoped>
+
+</style>
