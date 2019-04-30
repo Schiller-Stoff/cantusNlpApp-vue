de00380cf2f3fcaa85eecc0c9a9d8e9649c57093
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-30 15:52:20 +0200

    now using links from the table

--- cantus_advancedSearch/src/components/result/ResultTable.vue
+++ cantus_advancedSearch/src/components/result/ResultTable.vue
@@ -6,7 +6,17 @@
       <hr>
       <br>
       <br>
-      <v-client-table :data="refinedTableData" :columns="columns" :options="options"></v-client-table>
+      <v-client-table :data="refinedTableData" :columns="columns" :options="options">
+        <a slot="Fest" slot-scope="props" class="fa fa-edit" :href="props.row.Uri">
+          {{props.row.Fest}}
+        </a>
+        <a slot="Incipits" slot-scope="props" class="fa fa-edit" :href="props.row.i">
+          {{props.row.Incipits}}
+        </a>
+        <a slot="Hora" slot-scope="props" class="fa fa-edit" :href="props.row.o">
+          {{props.row.Hora}}
+        </a>
+      </v-client-table>
     </div>
 </template>
 
@@ -49,7 +59,7 @@
     data(){
       return {
         delayedDisplay:'hidden',
-        columns: ['Fest','Offizien', 'Incipits'],
+        columns: ['Fest','Hora', 'Gattungsbez', 'Incipits','Referenz'],
         options: {
           // see the options API
         }
@@ -64,7 +74,7 @@
         //TODO computed runs everytime when component created instead of re-ajaxed
         let refined = []
         for (let dataPoint of this.tableData){
-          refined.push({Fest: dataPoint.feastname,Offizien:dataPoint.office, Incipits:dataPoint.incipit})
+          refined.push({Fest: dataPoint.feastname,Hora:dataPoint.office, 'Gattungsbez':dataPoint.genre, Incipits:dataPoint.incipit,Referenz:dataPoint.r, Uri:dataPoint.feast, i:dataPoint.i, o:dataPoint.o })
         }
         return refined;
       },
@@ -84,6 +94,17 @@
         } else {
           this.delayedDisplay = ''
         }
+      }
+    },
+    methods: {
+      onRowClick(event){
+
+        // example for handling clicks here via js
+
+        //console.log(event)
+        //console.log(event.row.Uri)
+        //confirm("Zur Seite wechseln?")
+        //window.location.href = `${event.row.Uri}`
       }
     },
     created(){
