44e7e6c9393161510a9bdd6fc381f2f741635205
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-04-30 11:26:52 +0200

    experimental adding of an options object -> passed down to hide labels (to much info) and to only display / use the tooltips

--- cantus_advancedSearch/src/components/result/charts/LineChart.vue
+++ cantus_advancedSearch/src/components/result/charts/LineChart.vue
@@ -2,6 +2,23 @@
 <script>
   // CommitChart.js
   import {Line} from 'vue-chartjs'
+  let options = {
+    legend: {display:false},
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: { //hides the y axis
+      xAxes: [{
+        display: true,
+        ticks: {
+          display: false //this will remove only the label
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Suchergebnisse'
+        }
+      }]
+    }
+  }
 
   export default {
     extends: Line,
@@ -12,12 +29,12 @@
     },
     watch: {
       datasets(){
-        this.renderChart(this.chartData)
+        this.renderChart(this.chartData,options)
       }
     },
     mounted() {
       // Overwriting base render method with actual data.
-      this.renderChart(this.chartData)
+      this.renderChart(this.chartData,options)
     }
   }
 </script>
