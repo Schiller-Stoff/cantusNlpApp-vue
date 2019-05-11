a1fcce297f637ad04f2d0f587c9cd5f2ae9438cc
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-11 14:12:05 +0200

    completed structure of input fields + added incipitSearchParams as computed property to be emitted via event

--- cantus_advancedSearch/src/components/search/searchBar/TheIncipitSearch.vue
+++ cantus_advancedSearch/src/components/search/searchBar/TheIncipitSearch.vue
@@ -23,9 +23,62 @@
 
     <div class="input-group mb-3">
       <div class="input-group-prepend">
-        <label class="input-group-text" for="inputGroupSelect03">Genre</label>
+        <label class="input-group-text" for="inputGroupSelect02">Genre</label>
       </div>
-      <select v-model="chosenGenre" class="custom-select" id="inputGroupSelect03">
+      <select v-model="chosenGenre" class="custom-select" id="inputGroupSelect02">
+        <option selected value="default">Bitte wählen...</option>
+        <option value="AB">AB - Antiphon zum Benedictus</option>
+        <option value="AC">AC - Antiphon zur Commemoratio bzw. ad suffragium</option>
+        <option value="AD">AD - Antiphon zum Nunc dimittis</option>
+        <option value="AE">AE - Cantica-Antiphon (Evangelium)</option>
+        <option value="ALV">ALV - Alleluia-Vers</option>
+        <option value="AM">AM - Antiphon zum Magnificat</option>
+        <option value="AN">AN - Antiphon</option>
+        <option value="AP">AP - Prozessionsantiphon</option>
+        <option value="AS">AS - Marianische Schlussantiphon</option>
+
+        <option value="CO">CO - Communio</option>
+        <option value="CP">CP - Capitulum</option>
+
+        <option value="EP">EP - Epistel</option>
+        <option value="EPP">EPP - Epistel aus den Propheten</option>
+        <option value="EV">EV - Evangelium</option>
+
+        <option value="GR">GR - Graduale</option>
+
+        <option value="HY">HY - Hymnus</option>
+
+        <option value="IN">IN - Introitus</option>
+        <option value="INV">INV - Invitatorium</option>
+
+        <option value="LE">LE - Lectio (AT/NT)</option>
+        <option value="LH">LH - Homilie als Lectio</option>
+        <option value="LHI">LHI - Lectio aus Historia, Vita, Passio, Legenda</option>
+        <option value="LS">LS - Sermo als Lectio</option>
+
+        <option value="OF">OF - Offertorium</option>
+
+        <option value="PR">PR - Prosa</option>
+        <option value="PS">PS - Psalm</option>
+
+        <option value="RB">RB - Responsorium breve</option>
+        <option value="RP">RP - Responsorium prolixum</option>
+        <option value="RPP">RPP - Prozessionsresponsorium</option>
+
+        <option value="SE">SE - Sequenz</option>
+        <option value="TP">TP - Tropus</option>
+        <option value="TR">TR - Tractus</option>
+        <option value="VIS">VIS - Visitatio sepulchri</option>
+        <option value="VS">VS - Versikel</option>
+
+      </select>
+    </div>
+
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <label class="input-group-text" for="inputGroupSelect03">Hora</label>
+      </div>
+      <select v-model="chosenHora" class="custom-select" id="inputGroupSelect03">
         <option selected value="default">Bitte wählen...</option>
         <option value="AB">AB - Antiphon zum Benedictus</option>
         <option value="AC">AC - Antiphon zur Commemoratio bzw. ad suffragium</option>
@@ -76,14 +129,12 @@
 
     <br>
     <div>
-      <!--<h4>Zeitraum oder Fest</h4>-->
       <p>Wählen Sie einen <em>Zeitraum</em> oder ein...</p>
     </div>
     <div class="input-group mb-3">
       <div class="input-group-prepend">
-        <!--<label class="input-group-text" for="inputGroupSelect02">Zeitraum</label>-->
       </div>
-      <select v-model="curQueryObject" class="custom-select" id="inputGroupSelect02">
+      <select v-model="chosenTimeFrame" class="custom-select" id="inputGroupSelect04">
         <option selected value="default">Bitte wählen...</option>
         <option value="weihnachten">Weihnachten</option>
         <option value="vorfastenzeit">Vorfastenzeit</option>
@@ -99,17 +150,13 @@
       </select>
     </div>
 
-
-    <!--<h4>Query for Zeiträume AND Einzelfeste</h4>-->
-    <!--<p :class="fadeInAtEvent">{{blazeGraphQuery}}</p>-->
-
-    <!--<h4>Data Query for the "vergleichende Visualisierung"</h4>-->
-    <!--<p>{{dataQuery}}</p>-->
     <div>
       <p>...<em>einzelnes Fest</em>.</p>
-      <app-model-select :options="autoCompleteOptions" v-model="selectedFeast"></app-model-select>
+      <app-model-select :options="autoCompleteOptions" v-model="chosenFeast"></app-model-select>
       <br>
-      <button @click.prevent="searchResp(chosenGenre);searchVizCompareData('viz_setVizCompareDataAction')" class="btn btn-primary">Suche starten</button>
+      <button
+              class="btn btn-primary">Suche starten
+      </button>
     </div>
     <br>
 
@@ -120,9 +167,40 @@
 </template>
 
 <script>
-    export default {
-        name: "TheIncipitSearch"
+  //TODO: events --> startIncipitSearch / updateIncipitSearchParams? /
+  //TODO: props? --> none needed?
+  //TODO:
+
+  import { ModelSelect } from 'vue-search-select'
+  import {autocompleteVals} from "../../../data/autocompleteVals"
+
+  export default {
+    name: "TheIncipitSearch",
+    components: {
+      appModelSelect: ModelSelect,
+    },
+    data(){
+      return {
+        chosenLO:'',
+        chosenGenre:'',
+        chosenHora:'',
+        chosenTimeFrame:'',
+        chosenFeast:'',
+        autoCompleteOptions:autocompleteVals
+      }
+    },
+    computed: {
+      incipitSearchParams(){
+        return {
+          chosenLO: this.chosenLO,
+          chosenGenre: this.chosenGenre,
+          chosenHora: this.chosenHora,
+          chosenTimeFrame: this.chosenTimeFrame,
+          chosenFeast:this.chosenFeast
+        }
+      }
     }
+  }
 </script>
 
 <style scoped>
