480151b1b4d3dd9b37713d7dbe6b8f3f6d298734
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-13 09:41:06 +0200

    implemented filter for horae

--- cantus_advancedSearch/src/components/search/searchBar/TheIncipitSearch.vue
+++ cantus_advancedSearch/src/components/search/searchBar/TheIncipitSearch.vue
@@ -80,49 +80,22 @@
       </div>
       <select v-model="chosenHora" class="custom-select" id="inputGroupSelect03">
         <option selected value="default">Bitte wählen...</option>
-        <option value="AB">AB - Antiphon zum Benedictus</option>
-        <option value="AC">AC - Antiphon zur Commemoratio bzw. ad suffragium</option>
-        <option value="AD">AD - Antiphon zum Nunc dimittis</option>
-        <option value="AE">AE - Cantica-Antiphon (Evangelium)</option>
-        <option value="ALV">ALV - Alleluia-Vers</option>
-        <option value="AM">AM - Antiphon zum Magnificat</option>
-        <option value="AN">AN - Antiphon</option>
-        <option value="AP">AP - Prozessionsantiphon</option>
-        <option value="AS">AS - Marianische Schlussantiphon</option>
+        <option value="C">Completorium (C)</option>
 
-        <option value="CO">CO - Communio</option>
-        <option value="CP">CP - Capitulum</option>
+        <option value="L">Laudes (L)</option>
+        <option value="M">Matutinae (M)</option>
+        <option value="MI">Missa (MI)</option>
+        <option value="N">Nona (N)</option>
 
-        <option value="EP">EP - Epistel</option>
-        <option value="EPP">EPP - Epistel aus den Propheten</option>
-        <option value="EV">EV - Evangelium</option>
+        <option value="P">Prima (P)</option>
+        <option value="PRO">Prozessio (PRO)</option>
+        <option value="S">Sexta (S)</option>
 
-        <option value="GR">GR - Graduale</option>
+        <option value="T">Tertia (T)</option>
+        <option value="V">Vespera-1 (V)</option>
+        <option value="V2">Vespera-2 (V2)</option>
 
-        <option value="HY">HY - Hymnus</option>
 
-        <option value="IN">IN - Introitus</option>
-        <option value="INV">INV - Invitatorium</option>
-
-        <option value="LE">LE - Lectio (AT/NT)</option>
-        <option value="LH">LH - Homilie als Lectio</option>
-        <option value="LHI">LHI - Lectio aus Historia, Vita, Passio, Legenda</option>
-        <option value="LS">LS - Sermo als Lectio</option>
-
-        <option value="OF">OF - Offertorium</option>
-
-        <option value="PR">PR - Prosa</option>
-        <option value="PS">PS - Psalm</option>
-
-        <option value="RB">RB - Responsorium breve</option>
-        <option value="RP">RP - Responsorium prolixum</option>
-        <option value="RPP">RPP - Prozessionsresponsorium</option>
-
-        <option value="SE">SE - Sequenz</option>
-        <option value="TP">TP - Tropus</option>
-        <option value="TR">TR - Tractus</option>
-        <option value="VIS">VIS - Visitatio sepulchri</option>
-        <option value="VS">VS - Versikel</option>
 
       </select>
     </div>
@@ -167,6 +140,7 @@
 </template>
 
 <script>
+
   import { ModelSelect } from 'vue-search-select'
   import {autocompleteVals} from "../../../data/autocompleteVals"
   import {timeFrames} from '../../../data/timeFrameFeasts'
