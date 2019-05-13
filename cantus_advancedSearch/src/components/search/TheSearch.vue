1bd17fac62f24a4514fbde4e3f3eca945f8a49a4
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-13 19:08:09 +0200

    removed dead code

--- cantus_advancedSearch/src/components/search/TheSearch.vue
+++ cantus_advancedSearch/src/components/search/TheSearch.vue
@@ -46,41 +46,7 @@
     computed: {
       incipitQuery(val){
         if(val===undefined)return;
-        //this.$store.dispatch('search_setSearchParamsAction',searchParams)
-
-        //testing failing query
-        if(this.chosenLO==='FAIL_QUERY')return '9$$ß34'
-
-        //for testing delayed respinse
-        if(this.chosenLO==='/api/users?delay=5')return 'https://reqres.in/api/users?delay=5'
-        if(this.chosenLO==='/api/users?delay=12')return 'https://reqres.in/api/users?delay=12'
-
-        //for resp.test
-        if(this.chosenLO==='passau') return `https://${this.server}/archive/objects/query:resp.test/methods/sdef:Query/getJSON?params=%241%7C${this.incipitSearchParams.chosenGenre}`;
-
-        // return correct query
-        // takes feasts numbers in "feasts" array and build correct SPARQL via string concatination
-        // a UNION triple-statement
-        // if(this.feasts.length > 1){
-        //   let buildQuery = `https://${this.server}/archive/objects/query:cantus.genres/methods/sdef:Query/getJSON?params=%241%7C%3Chttps%3A%2F%2Fgams.uni-graz.at%2Fo%3Acantus.${this.incipitSearchParams.chosenLO}%3E%3B%242%7C${this.incipitSearchParams.chosenGenre}%3B%243%7C`
-        //   for (let i = 0; i < this.feasts.length; i++) {
-        //     if(i===0){
-        //       buildQuery += `%7B%3Ffeast%20cantus%3AfeastCode%20%22${this.feasts[i]}%22%7D`
-        //     } else {
-        //       buildQuery += `%20UNION%20%7B%3Ffeast%20cantus%3AfeastCode%20%22${this.feasts[i]}%22%7D`
-        //     }
-        //   }
-        //   //this.$store.dispatch('search_modifyCurSearchQueryAction',buildQuery)
-        //   return buildQuery
-        // } else {
-        //   let query = `https://${this.server}/archive/objects/query:cantus.genres/methods/sdef:Query/getJSON?params=%241%7C%3Chttps%3A%2F%2Fgams.uni-graz.at%2Fo%3Acantus.${this.chosenLO}%3E%3B%242%7C${this.chosenGenre}%3B%243%7C%7B%3Ffeast%20cantus%3AfeastCode%20%22${this.selectedFeast.value}%22%7D`
-        //   //this.$store.dispatch('search_modifyCurSearchQueryAction',query)
-        //   return query
-        // }
-
-
         let queryStart = `${this.urlStart}/query:cantus.genres/methods/sdef:Query/getJSON?params=`
-
         let queryParams =
           `$1|<https://gams.uni-graz.at/o:cantus.${this.incipitSearchParams.chosenLO}>;` +
           `$2|${this.incipitSearchParams.chosenGenre};` +
@@ -91,11 +57,6 @@
       }
     },
     methods: {
-      logToConsole(data){
-        console.log("test method called!")
-        console.log(data)
-      },
-
       saveIncipitSearchData(data){
         console.debug('saveIncipitSearchData...')
         this.incipitSearchParams = data
@@ -139,11 +100,9 @@
           this.$store.dispatch('incipit_markOngoingSearchAction', false)
         });
       },
-
       encodeUri(uri){
         return encodeURI(uri)
       }
-
     }
   }
 </script>
