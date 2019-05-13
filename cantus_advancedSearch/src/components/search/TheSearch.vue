f00d9f82628dca6ff16681e93480a8c7c9b5f5e4
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-13 08:50:23 +0200

    started to implement the incipit search query

--- cantus_advancedSearch/src/components/search/TheSearch.vue
+++ cantus_advancedSearch/src/components/search/TheSearch.vue
@@ -1,13 +1,19 @@
 <template>
-  <app-the-search-bar
-    @routed_startIncipitSearch="logToConsole($event)"
-    @routed_incipitParamsUpdated="logToConsole($event)"
-  ></app-the-search-bar>
+  <div>
+    <app-the-search-bar
+      @routed_startIncipitSearch="logToConsole($event)"
+      @routed_incipitParamsUpdated="logToConsole($event);saveIncipitSearchData($event)"
+    ></app-the-search-bar>
+
+    <p>{{incipitQuery}}</p>
+
+  </div>
 </template>
 
 <script>
   //TODO split functionality between TheSearch component (ajax etc.) and TheSearchBar (gui etc.)
   //TODO think about event emiting / handling and props
+  //TODO implement incipitSearch
 
   import TheSearchBar from './TheSearchBar'
 
@@ -16,11 +22,68 @@
     components: {
       appTheSearchBar: TheSearchBar
     },
+    data(){
+      return {
+        server:'glossa.uni-graz.at',
+        incipitSearchParams: {
+          chosenLO: 'passau.ur',
+          chosenGenre: 'RP',
+          chosenHora: '',
+          chosenTimeFrame: '',
+          chosenFeast:'01013000'
+        }
+      }
+    },
+    computed: {
+      incipitQuery(val){
+        if(val===undefined)return;
+        //this.$store.dispatch('search_setSearchParamsAction',searchParams)
+
+        //testing failing query
+        if(this.chosenLO==='FAIL_QUERY')return '9$$ß34'
+
+        //for testing delayed respinse
+        if(this.chosenLO==='/api/users?delay=5')return 'https://reqres.in/api/users?delay=5'
+        if(this.chosenLO==='/api/users?delay=12')return 'https://reqres.in/api/users?delay=12'
+
+        //for resp.test
+        if(this.chosenLO==='passau') return `https://${this.server}/archive/objects/query:resp.test/methods/sdef:Query/getJSON?params=%241%7C${this.incipitSearchParams.chosenGenre}`;
+
+        // return correct query
+        // takes feasts numbers in "feasts" array and build correct SPARQL via string concatination
+        // a UNION triple-statement
+        // if(this.feasts.length > 1){
+        //   let buildQuery = `https://${this.server}/archive/objects/query:cantus.genres/methods/sdef:Query/getJSON?params=%241%7C%3Chttps%3A%2F%2Fgams.uni-graz.at%2Fo%3Acantus.${this.incipitSearchParams.chosenLO}%3E%3B%242%7C${this.incipitSearchParams.chosenGenre}%3B%243%7C`
+        //   for (let i = 0; i < this.feasts.length; i++) {
+        //     if(i===0){
+        //       buildQuery += `%7B%3Ffeast%20cantus%3AfeastCode%20%22${this.feasts[i]}%22%7D`
+        //     } else {
+        //       buildQuery += `%20UNION%20%7B%3Ffeast%20cantus%3AfeastCode%20%22${this.feasts[i]}%22%7D`
+        //     }
+        //   }
+        //   //this.$store.dispatch('search_modifyCurSearchQueryAction',buildQuery)
+        //   return buildQuery
+        // } else {
+        //   let query = `https://${this.server}/archive/objects/query:cantus.genres/methods/sdef:Query/getJSON?params=%241%7C%3Chttps%3A%2F%2Fgams.uni-graz.at%2Fo%3Acantus.${this.chosenLO}%3E%3B%242%7C${this.chosenGenre}%3B%243%7C%7B%3Ffeast%20cantus%3AfeastCode%20%22${this.selectedFeast.value}%22%7D`
+        //   //this.$store.dispatch('search_modifyCurSearchQueryAction',query)
+        //   return query
+        // }
+
+        return `${this.incipitSearchParams.chosenLO} + ${this.incipitSearchParams.chosenGenre} + ${this.incipitSearchParams.chosenHora} + ${this.incipitSearchParams.chosenTimeFrame} + ${this.incipitSearchParams.chosenFeast}`
+
+
+      }
+    },
     methods: {
       logToConsole(data){
         console.log("test method called!")
         console.log(data)
       },
+
+      saveIncipitSearchData(data){
+        this.incipitSearchParams = data
+      }
+
     }
   }
 </script>
