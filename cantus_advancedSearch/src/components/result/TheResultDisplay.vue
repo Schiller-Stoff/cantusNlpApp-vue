709503469a755c12b7343aa64d2da8f3200ec5fd
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-15 11:38:02 +0200

    added props first

--- cantus_advancedSearch/src/components/result/TheResultDisplay.vue
+++ cantus_advancedSearch/src/components/result/TheResultDisplay.vue
@@ -3,9 +3,23 @@
 </template>
 
 <script>
-    export default {
-        name: "TheResultDisplay"
+  export default {
+    name: "TheResultDisplay",
+    props:{
+      incipitSearch:{
+        type:required
+      },
+      incipitOngoingSearch:{
+        type:required
+      },
+      incipitSearchFailed:{
+        type:required
+      },
+      incipitSearchHistory: {
+        type:required
+      }
     }
+  }
 </script>
 
 <style scoped>
