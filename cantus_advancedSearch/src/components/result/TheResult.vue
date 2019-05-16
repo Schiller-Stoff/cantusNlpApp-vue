d129d945e2c06719c05813426cfee621ce250b0f
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-16 08:23:34 +0200

    bigger refactor -> incipit viz related data is now refactored in TheResultDisplay, TheResult is only responsible for receiving the result data and passing along the data with useful references / TheResult communicates with vueX alone

--- cantus_advancedSearch/src/components/result/TheResult.vue
+++ cantus_advancedSearch/src/components/result/TheResult.vue
@@ -1,129 +1,35 @@
 <template>
-  <div class="container-fluid">
-    <app-the-result-default
-      :key="0"
-      v-if="!incipitSearchHistory[0] && !incipitOnGoingSearch && !incipitSearchFailed">
-    </app-the-result-default>
-    <app-the-result-preview
-      v-if="incipitSearchHistory[0] && !incipitOnGoingSearch && !incipitSearchFailed && !searchBarLocked"
-      :searchParams="incipitSearch.searchParams"
-      :vizHistoData="incipitVizHistory"
-      :key="1">
-    </app-the-result-preview>
-    <app-the-result-table
-      v-if="incipitSearchHistory[0] && !incipitOnGoingSearch && !incipitSearchFailed && searchBarLocked"
-      :key="2"
-      :tableData="incipitSearch.response.body"
-      :searchParams="incipitSearch.searchParams">
-    </app-the-result-table>
-    <app-the-result-load-handler
-      v-if="incipitOnGoingSearch || incipitSearchFailed"
-      :key="4"
-      :searchFailed="incipitSearchFailed"
-    >
-    </app-the-result-load-handler>
-
-    <button class="btn btn-primary" @click="toggleInterfaceLock(true)">lock</button>
-    <button class="btn btn-primary" @click="toggleInterfaceLock(false)">unlock</button>
-
-  </div>
+  <app-the-result-display
+    :incipitSearch="incipitSearch"
+    :incipitOngoingSearch="incipitOngoingSearch"
+    :incipitSearchFailed="incipitSearchFailed"
+    :incipitSearchHistory="incipitSearchHistory"
+  ></app-the-result-display>
 </template>
 
 <script>
   //TODO remove curChartData variable --- I think it's unnecessary?
   //TODO vuex result namings are wrong now -> e.g. searchHistory -> to incipitSearchHistory
-  //TODO move gui functionality to TheResultDisplay.vue
-  //TODO pass in data via props and events / vuex only in TheResult.vue
-  //TODO remove old + dead code
   //TODO Complete renaming and hierarchy
 
-  let curTimer;
-  import {mapActions} from 'vuex'
   import {mapGetters} from 'vuex'
-  import TheResultTable from './TheResultTable'
-  import TheResultPreview from './compare/TheResultPreview'
-  import TheResultDefault from './TheResultDefault'
-  import TheResultLoadHandler from './TheResultLoadHandler'
+  import TheResultDisplay from './TheResultDisplay'
 
   export default {
     name: "TheResult.vue",
-    data() {
-      return {
-        incipitDiagramData:undefined, // "one bar" (but there might be other inline-bars like segments for comparison inside one diagram)
-        incipitChartData:undefined,   // "complete chart" with multiple bars next to each other
-        incipitVizHistory:[],
-
-        // data of 'last/current' incipitSearch
-        incipitSearch:''
-      }
-    },
     computed: {
       ...mapGetters({
-        searchBarLocked:'interfaceStates_currentSearchLockState',
-        incipitOnGoingSearch:'incipit_getOngoingSearch',
+        incipitOngoingSearch:'incipit_getOngoingSearch',
         incipitSearchFailed:'incipit_getSearchFailed',
         incipitSearchHistory:'incipit_getSearchHistory'
-      })
-    },
-    watch: {
-      incipitSearchHistory(){
-        //save reference to last/current incipitSearch
-        this.incipitSearch = this.incipitSearchHistory[this.incipitSearchHistory.length-1]
-
-        //set viz Data
-        this.calcCurDiagramData()
-        this.calcCurChartData()
-
-        //push into viz history
-        this.incipitChartData.searchParams = this.incipitSearch.searchParams
-        this.incipitVizHistory.push(this.incipitChartData)
+      }),
+      incipitSearch(){
+        let length = this.incipitSearchHistory.length
+        return this.incipitSearchHistory[length-1]
       }
     },
     components: {
-      appTheResultPreview: TheResultPreview,
-      appTheResultTable: TheResultTable,
-      appTheResultDefault: TheResultDefault,
-      appTheResultLoadHandler: TheResultLoadHandler
-    },
-    methods: {
-      ...mapActions({
-        lockInterface: 'interfaceStatesAction_lockSearchbar',
-        unlockInterface:'interfaceStatesAction_unlockSearchbar'
-      }),
-      toggleInterfaceLock(lock_boolean){
-        // uses lockInterface() method -> calls vuex-actions
-        if(lock_boolean){
-          this.lockInterface()
-        } else {
-          this.unlockInterface()
-        }
-      },
-      randomColor() {
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-      },
-      calcCurDiagramData(){
-        let resultCount = this.incipitSearch.response.body.length
-        let vizObj = {
-          label:'Feste',
-          backgroundColor: [],
-          data: [resultCount]
-        }
-        for (let dp of vizObj.data){
-          vizObj.backgroundColor.push(this.randomColor())
-        }
-        this.incipitDiagramData = vizObj
-      },
-      calcCurChartData(){
-        this.incipitChartData = {
-          labels:[this.incipitSearch.searchParams.chosenGenre, 'Rest'],
-          datasets: [this.incipitDiagramData]
-        }
-      }
+      appTheResultDisplay: TheResultDisplay
     }
   }
 </script>
--- cantus_advancedSearch/src/components/result/TheResultDisplay.vue
+++ cantus_advancedSearch/src/components/result/TheResultDisplay.vue
@@ -1,25 +1,106 @@
 <template>
+  <div class="container-fluid">
+    <app-the-result-default
+      :key="0"
+      v-if="!incipitSearchHistory[0] && !incipitOngoingSearch && !incipitSearchFailed">
+    </app-the-result-default>
+    <app-the-result-preview
+      v-if="incipitSearchHistory[0] && !incipitOngoingSearch && !incipitSearchFailed && !showTable"
+      :searchParams="incipitSearch.searchParams"
+      :vizHistoData="incipitVizHistory"
+      :key="1">
+    </app-the-result-preview>
+    <app-the-result-table
+      v-if="incipitSearchHistory[0] && !incipitOngoingSearch && !incipitSearchFailed && showTable"
+      :key="2"
+      :tableData="incipitSearch.response.body"
+      :searchParams="incipitSearch.searchParams">
+    </app-the-result-table>
+    <app-the-result-load-handler
+      v-if="incipitOngoingSearch || incipitSearchFailed"
+      :key="3"
+      :searchFailed="incipitSearchFailed"
+    >
+    </app-the-result-load-handler>
 
+    <button class="btn btn-primary" @click="toggleTable">switch</button>
+  </div>
 </template>
 
 <script>
   //TODO implement viz Data refactoring inside here!
 
+  import TheResultDefault from './TheResultDefault'
+  import TheResultTable from './TheResultTable'
+  import TheResultLoadHandler from './TheResultLoadHandler'
+  import TheResultPreview from './compare/TheResultPreview'
+
   export default {
     name: "TheResultDisplay",
+    components: {
+      appTheResultDefault:TheResultDefault,
+      appTheResultTable:TheResultTable,
+      appTheResultLoadHandler: TheResultLoadHandler,
+      appTheResultPreview: TheResultPreview
+    },
     props:{
       incipitSearch:{
-        type:required
+        required:true
       },
       incipitOngoingSearch:{
-        type:required
+        required:true
       },
       incipitSearchFailed:{
-        type:required
+        required:true
       },
       incipitSearchHistory: {
-        type:required
+        required:true
       }
+    },
+    data(){
+      return {
+        showTable:false,
+        incipitVizHistory:[]
+      }
+    },
+    computed: {
+      incipitDiagramData(){
+        let resultCount = this.incipitSearch.response.body.length
+        return {
+          label:'Incipits',
+          backgroundColor: [this.randomColor()],
+          data: [resultCount]
+        };
+      },
+      incipitChartData(){
+        return {
+          labels:[this.incipitSearch.searchParams.chosenGenre, 'Rest'],
+          datasets: [this.incipitDiagramData],
+          searchParams:this.incipitSearch.searchParams
+        }
+      }
+    },
+    watch: {
+      incipitSearchHistory:{
+        deep:true,
+        handler(){
+          this.incipitVizHistory.push(this.incipitChartData)
+          console.log(this.incipitVizHistory)
+        }
+      }
+    },
+    methods: {
+      toggleTable(){
+        return this.showTable = !this.showTable
+      },
+      randomColor() {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      },
     }
   }
 </script>
