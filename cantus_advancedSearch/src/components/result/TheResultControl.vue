1569edfd3afc6057475848ccd3cd8986b64c4297
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-20 13:32:51 +0200

    added the ResultControl component

---
+++ cantus_advancedSearch/src/components/result/TheResultControl.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="TheResultControl_mainContainer">
+    <ul class="list-group list-inline text-right">
+      <li class="list-group-item">
+        <i class="fas fa-chart-bar"></i>
+      </li>
+      <li class="list-group-item">
+        <i class="far fa-file-alt"></i>
+      </li>
+      <li class="list-group-item">
+        <i class="far fa-window-maximize"></i>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TheResultControl"
+  }
+</script>
+
+<style scoped lang="scss">
+  @import "../../scss/globalVariables/globalVariables";
+
+  .TheResultControl_mainContainer {
+    background-color: $primaryColor;
+  }
+
+  i {
+    font-size: 1.75em;
+    padding: .25em;
+  }
+
+  li {
+    border:none;
+  }
+
+  ul {
+    position: sticky !important;
+  }
+
+</style>
