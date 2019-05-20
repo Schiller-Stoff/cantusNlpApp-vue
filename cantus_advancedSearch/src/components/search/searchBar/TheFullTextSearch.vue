e39a1dacb85ef7730a34f9f1a03c41030e8a532c
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-20 17:58:14 +0200

    started to implement libriOrdinarii.js -> where I can select / choose lbiri ordinari text and search param value!

--- cantus_advancedSearch/src/components/search/searchBar/TheFullTextSearch.vue
+++ cantus_advancedSearch/src/components/search/searchBar/TheFullTextSearch.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <h4>Erweiterte Volltextsuche</h4>
+
+
   </div>
 </template>
 
 <script>
-    export default {
-        name: "TheFullTextSearch"
-    }
+  //TODO import selection values via mixins! (e.g. like selection for the LOs etc.) -> proceed with libriOrdinarii.js!
+
+  export default {
+    name: "TheFullTextSearch"
+  }
 </script>
 
 <style scoped>
---
+++ cantus_advancedSearch/src/data/libriOrdinarii.js
@@ -0,0 +1,14 @@
+export const libriOrdinarii = {
+  'Dioezese Brixen - Edition I-SCAcc VII A 10':'o:cantus.brixen',
+  'Dioezese Freising -Edition D-Mu 4 Cod. 157':'o:cantus.freising',
+  'Hirsau (OSB) - Edition CH-Zz RH 74b':'o:cantus.hirsau',
+
+  'Klosteneuburg (CRSA) - Edition KNB1 (A-KN 635)':'o:cantus.klosterneuburg.knb1',
+  'Klosteneuburg (CRSA) - Edition KNB2 (A-KN 1213)':'o:cantus.klosterneuburg.knb2',
+  'Klosteneuburg (CRSA) - Edition KNB3 (A-KN 983)':'o:cantus.klosterneuburg.knb3',
+  'Klosteneuburg (CRSA) - Edition KNB4 (A-KN 1014)':'o:cantus.klosterneuburg.knb4',
+
+  'Moosburg (Kollegiatsstift) - Edition D-Mbs Clm 9469':'o:cantus.mossburg',
+
+
+}
