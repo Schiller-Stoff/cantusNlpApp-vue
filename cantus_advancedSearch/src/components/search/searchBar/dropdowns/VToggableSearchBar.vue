1f0fc4aed1e9456615934dbb61e497d8721046f9
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-22 09:37:43 +0200

    added VToggableSearchBar component

---
+++ cantus_advancedSearch/src/components/search/searchBar/dropdowns/VToggableSearchBar.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="input-group">
+    <span
+      class="input-group-addon"
+      id="basic-addon1"
+      @click="searchToggable ? toggleSearchField() : ''"
+
+    >{{showSearch ? searchButton.textActive : searchButton.textInActive}}</span>
+
+    <select
+      v-model="value"
+      class="form-control"
+      id="inputGroupSelect03"
+      @change="vModelEmit(value)"
+      v-if="searchToggable ? showSearch : true">
+
+      <option
+        v-for="option in options"
+        :value="option"
+      >{{option.text}}
+      </option>
+
+    </select>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "VToggableSearchBar",
+    props: {
+      searchButton:{
+        type:Object,
+        required:true,
+        validator(value){
+          let demandedKeys = ['textActive','textInActive']
+          for (let key of demandedKeys){
+            try {
+              if(value[key]===undefined)throw new Error();
+            } catch (e) {
+              console.error('Key not found in searchButton prop for VToggableSearchBar.vue. Missing key: ' + key);
+              return false;
+            }
+          }
+          return true;
+        }
+      },
+      options: {
+        type: Array,
+        required: true,
+        validator(value){
+          let demandedKeys = ['text','value']
+          for (let key of demandedKeys){
+            try {
+              if(value[0][key]===undefined)throw new Error();
+            } catch (e) {
+              console.error('Key not found in options prop for VToggableSearchBar.vue. Missing key: ' + key);
+              return false;
+            }
+          }
+          return true;
+        }
+      },
+      searchToggable: {
+        required: true,
+        default: false
+      }
+    },
+    data(){
+      return {
+        showSearch: !this.searchToggable,
+        value:''
+      }
+    },
+    methods:{
+      toggleSearchField(){
+        this.showSearch = !this.showSearch
+        this.$emit('searchFieldToggled',this.showSearch)
+      },
+      vModelEmit(val){
+        this.$emit('input',val)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
