43566eeaff50d5aa63204c1084638c57a0991b79
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-23 12:29:27 +0200

    implemented base styling

--- cantus_advancedSearch/src/components/search/searchBar/TheFullTextIncipitSearch.vue
+++ cantus_advancedSearch/src/components/search/searchBar/TheFullTextIncipitSearch.vue
@@ -1,13 +1,47 @@
 <template>
-    <h1>Hi</h1>
+  <div class="container-fluid TheFullTextIncipitSearch_mainContainer">
+    <h4>Erweiterte Volltextsuche (incipit)</h4>
+    <br>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "TheFullTextIncipitSearch"
-    }
+  export default {
+    name: "TheFullTextIncipitSearch"
+  }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import "../../../scss/globalVariables/globalVariables";
+
+  .TheFullTextIncipitSearch_input {
+    width:65% !important;
+  }
+
+
+  .TheFullTextIncipitSearch_mainContainer {
+    /*padding:4em 2em 2em 2em;*/
+    /*border: lightgrey solid .15em;*/
+    /*margin:1em;*/
+    border-radius: .25em;
+    background-color: $primaryColor;
+  }
+
+
+  p {
+    text-align: justify;
+  }
+
+  hr {
+    border: none;
+    padding:.25em;
+    background-color: lightgrey;
+    border-radius: 5em;
+  }
+
+  h4 {
+    text-decoration: underline;
+    /*color: $tertiaryColor*/
+  }
 
 </style>
