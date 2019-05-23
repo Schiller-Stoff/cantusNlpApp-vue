3a71bf7a048433bac547ca612b2e4b6c042d59f9
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-23 11:18:06 +0200

    now using ToggleSearchBar also in the VGenreSelect

--- cantus_advancedSearch/src/components/search/searchBar/dropdowns/VGenreSelect.vue
+++ cantus_advancedSearch/src/components/search/searchBar/dropdowns/VGenreSelect.vue
@@ -1,20 +1,44 @@
 <template>
-  <div class="input-group mb-3">
-    <div class="input-group-prepend">
-      <label class="input-group-text" for="inputGroupSelect02">Genre</label>
-    </div>
-    <select v-model="value" class="custom-select" id="inputGroupSelect02" @change="emitVal(value)">
-      <option v-for="genre in genres" :value="genre.value">{{genre.text}}</option>
-    </select>
+  <div>
+    <!--<div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <label class="input-group-text" for="inputGroupSelect02">Genre</label>
+      </div>
+      <select v-model="value" class="custom-select" id="inputGroupSelect02" @change="emitVal(value)">
+        <option v-for="genre in genres" :value="genre.value">{{genre.text}}</option>
+      </select>
+    </div>-->
+
+    <app-v-toggable-search-bar
+      v-model="value"
+      :options="genres"
+      :searchButton="{textActive:'Genre', textInActive:'Genre einschränken'}"
+      :searchToggable="searchToggable"
+      @input="vModelEmit(value.value)"
+      @searchFieldToggled="emitSearchFieldShownStatus($event)"
+
+    ></app-v-toggable-search-bar>
+
+
   </div>
 </template>
 
 <script>
 
   import {genres} from "../../../../data/genres";
+  import VToggableSearchBar from './VToggableSearchBar'
 
   export default {
     name: "VGenreSelect",
+    components: {
+      appVToggableSearchBar:VToggableSearchBar
+    },
+    props: {
+      searchToggable:{
+        default:false,
+        type:Boolean
+      }
+    },
     data(){
       return {
         value:'',
@@ -22,8 +46,11 @@
       }
     },
     methods: {
-      emitVal(val){
-        this.$emit('input',val)
+      vModelEmit(value){
+        this.$emit('input',value)
+      },
+      emitSearchFieldShownStatus(value){
+        this.$emit('searchFieldToggled', value)
       }
     }
   }
