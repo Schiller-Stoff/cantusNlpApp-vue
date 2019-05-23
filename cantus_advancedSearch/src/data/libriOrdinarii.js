17a708c893cd657e0a7b0c697e77a315ab88f24a
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-23 14:42:51 +0200

    removed 'Edition'

--- cantus_advancedSearch/src/data/libriOrdinarii.js
+++ cantus_advancedSearch/src/data/libriOrdinarii.js
@@ -1,40 +1,40 @@
 export const libriOrdinarii = [
-  {text:'Dioezese Brixen - Edition I-SCAcc VII A 10',value:'o:cantus.brixen'},
+  {text:'Dioezese Brixen I-SCAcc VII A 10',value:'o:cantus.brixen'},
   {text:'Dioezese Freising -Edition D-Mu 4 Cod. 157',value:'o:cantus.freising'},
-  {text:'Hirsau (OSB) - Edition CH-Zz RH 74b',value:'o:cantus.hirsau'},
+  {text:'Hirsau (OSB) CH-Zz RH 74b',value:'o:cantus.hirsau'},
 
-  {text:'Klosteneuburg (CRSA) - Edition KNB1 (A-KN 635)',value:'o:cantus.klosterneuburg.knb1'},
-  {text:'Klosteneuburg (CRSA) - Edition KNB2 (A-KN 1213)',value:'o:cantus.klosterneuburg.knb2'},
-  {text:'Klosteneuburg (CRSA) - Edition KNB3 (A-KN 983)',value:'o:cantus.klosterneuburg.knb3'},
-  {text:'Klosteneuburg (CRSA) - Edition KNB4 (A-KN 1014)',value:'o:cantus.klosterneuburg.knb4'},
+  {text:'Klosteneuburg (CRSA) KNB1 (A-KN 635)',value:'o:cantus.klosterneuburg.knb1'},
+  {text:'Klosteneuburg (CRSA) KNB2 (A-KN 1213)',value:'o:cantus.klosterneuburg.knb2'},
+  {text:'Klosteneuburg (CRSA) KNB3 (A-KN 983)',value:'o:cantus.klosterneuburg.knb3'},
+  {text:'Klosteneuburg (CRSA) KNB4 (A-KN 1014)',value:'o:cantus.klosterneuburg.knb4'},
 
-  {text:'Moosburg (Kollegiatsstift) - Edition D-Mbs Clm 9469',value:'o:cantus.moosburg'},
+  {text:'Moosburg (Kollegiatsstift) D-Mbs Clm 9469',value:'o:cantus.moosburg'},
 
-  {text:'Dioezese Passau - Edition rekonstruierter Urtext',value:'o:cantus.passau.ur'},
-  {text:'Dioezese Passau - Edition Edition SP1 (A-SPL 83/3)',value:'o:cantus.passau.sp'},
-  {text:'Dioezese Passau - Edition SVE (A-Wn 1874)',value:'o:cantus.passau.sve'},
-  {text:'Dioezese Passau - Edition KNB (A-KN 1194)',value:'o:cantus.passau.knb'},
+  {text:'Dioezese Passau rekonstruierter Urtext',value:'o:cantus.passau.ur'},
+  {text:'Dioezese Passau Edition SP1 (A-SPL 83/3)',value:'o:cantus.passau.sp'},
+  {text:'Dioezese Passau SVE (A-Wn 1874)',value:'o:cantus.passau.sve'},
+  {text:'Dioezese Passau KNB (A-KN 1194)',value:'o:cantus.passau.knb'},
 
-  {text:'Dioezese Passau (CRSA) - Edition PAC (A-Gu 208)',value:'o:cantus.seckau1.pac'},
+  {text:'Dioezese Passau (CRSA) PAC (A-Gu 208)',value:'o:cantus.seckau1.pac'},
 
-  {text:'St.Nikola (CRSA) - Edition Mengotus (A-Wn 1482)',value:'o:cantus.stnikola'},
+  {text:'St.Nikola (CRSA) Mengotus (A-Wn 1482)',value:'o:cantus.stnikola'},
 
-  {text:'Dioesese Regensburg - Edition D-Mbs clm 26947',value:'o:cantus.regensburg'},
+  {text:'Dioesese Regensburg D-Mbs clm 26947',value:'o:cantus.regensburg'},
 
-  {text:'St.Emmeram (OSB) - Edition EMM1 (D-Mbs clm 14183)',value:'o:cantus.emmeram.emm1'},
-  {text:'St.Emmeram (OSB) - Edition EMM2 (D-Mbs clm 14428)',value:'o:cantus.emmeram.emm2'},
-  {text:'St.Emmeram (OSB) - Edition EMM3 (D-Mbs clm 14073, Kastl)',value:'o:cantus.emmeram.emm3'},
+  {text:'St.Emmeram (OSB) EMM1 (D-Mbs clm 14183)',value:'o:cantus.emmeram.emm1'},
+  {text:'St.Emmeram (OSB) EMM2 (D-Mbs clm 14428)',value:'o:cantus.emmeram.emm2'},
+  {text:'St.Emmeram (OSB) EMM3 (D-Mbs clm 14073, Kastl)',value:'o:cantus.emmeram.emm3'},
 
-  {text:'Erzdioezese Salzburg - Edition rekonstruierter Urtext',value:'o:cantus.salzburg.ur'},
-  {text:'Erzdioezese Salzburg - Edition SAL (A-Su M II 6)',value:'o:cantus.salzburg.sal'},
-  {text:'Erzdioezese Salzburg - Edition RA (D-Mbs clm 12635)',value:'o:cantus.salzburg.ra'},
-  {text:'Erzdioezese Salzburg - Edition VO (A-VOR 99)',value:'o:cantus.salzburg.vo'},
+  {text:'Erzdioezese Salzburg rekonstruierter Urtext',value:'o:cantus.salzburg.ur'},
+  {text:'Erzdioezese Salzburg SAL (A-Su M II 6)',value:'o:cantus.salzburg.sal'},
+  {text:'Erzdioezese Salzburg RA (D-Mbs clm 12635)',value:'o:cantus.salzburg.ra'},
+  {text:'Erzdioezese Salzburg VO (A-VOR 99)',value:'o:cantus.salzburg.vo'},
 
-  {text:'Seckau (CRSA) - Edition Seckau 1 (A-Gu 208)',value:'o:cantus.seckau1.se1'},
-  {text:'Seckau (CRSA) - Edition Seckau 2',value:'o:cantus.seckau2'},
-  {text:'Seckau (CRSA) - Edition Seckau 3 (A-Gu 1566)',value:'o:cantus.seckau3'},
+  {text:'Seckau (CRSA) Seckau 1 (A-Gu 208)',value:'o:cantus.seckau1.se1'},
+  {text:'Seckau (CRSA) Seckau 2',value:'o:cantus.seckau2'},
+  {text:'Seckau (CRSA) Seckau 3 (A-Gu 1566)',value:'o:cantus.seckau3'},
 
-  {text:'Vorau-Salzburg (CRSA) - Edition A-VOR 333',value:'o:cantus.vorau'}
+  {text:'Vorau-Salzburg (CRSA) A-VOR 333',value:'o:cantus.vorau'}
 
 
 ]
