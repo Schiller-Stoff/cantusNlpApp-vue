e5cd4a06f0965fbaf92eb88f1051bd6696980494
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-27 15:05:12 +0200

    changed the manual text a bit

--- cantus_advancedSearch/src/components/search/searchBar/TheSearchHelpText.vue
+++ cantus_advancedSearch/src/components/search/searchBar/TheSearchHelpText.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="container-fluid">
       <h4>Wilkommen zur erweiterten Suche von Cantus</h4>
+      <p>Die erweiterte Suche von Cantus besteht aus drei unterschiedlichen Suchmodi... </p>
+
+      <p>RDF-Daten via SPARQL abgefragt...Ergebnisse hier in diesem Tool visualisiert...</p>
       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
 
       <br>
-      <h5>Zur Verfügung stehende Suchtypen:</h5>
+      <h5>Suchmodi:</h5>
       <ol>
         <li>
           <p>Erweiterte Volltextsuche</p>
