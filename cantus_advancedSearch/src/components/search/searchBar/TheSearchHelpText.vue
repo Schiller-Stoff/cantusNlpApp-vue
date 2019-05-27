76918bcb2180d1ad43bb63f0584dd00f9858cb61
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-27 12:34:04 +0200

    expanded descriptive text

--- cantus_advancedSearch/src/components/search/searchBar/TheSearchHelpText.vue
+++ cantus_advancedSearch/src/components/search/searchBar/TheSearchHelpText.vue
@@ -4,7 +4,22 @@
       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
 
       <br>
-      <h5>Genre</h5>
+      <h5>Zur Verfügung stehende Suchtypen:</h5>
+      <ol>
+        <li>
+          <p>Erweiterte Volltextsuche</p>
+        </li>
+        <li>
+          <p>Erweiterte Volltextsuche auf Basis von Incipits</p>
+        </li>
+        <li>
+          <p>Erweiterte Incipitsuche</p>
+        </li>
+      </ol>
+      <br>
+
+
+
       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
     </div>
@@ -16,6 +31,16 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+  hr {
+    border: none;
+    padding:.25em;
+    background-color: lightgrey;
+    border-radius: 5em;
+  }
+
+  h5 {
+    text-decoration: underline;
+  }
 </style>
