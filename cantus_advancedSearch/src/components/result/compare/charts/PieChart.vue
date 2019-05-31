c2282817c127eeccb2f5855a98e558578da21996
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-31 16:43:20 +0200

    the pie chart now also has meaningful options

--- cantus_advancedSearch/src/components/result/compare/charts/PieChart.vue
+++ cantus_advancedSearch/src/components/result/compare/charts/PieChart.vue
@@ -1,25 +1,60 @@
-
 <!--HERE IS NO TEMPLATE ALLOWED!-->
 
 
 <script>
-    import {Pie} from 'vue-chartjs'
-    export default {
-      extends: Pie,
-      props:{
-        chartData: {
-          type:Object //TODO add some validation
+  let options = {
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    tooltips: {
+      //for displaying tooltips
+    },
+    scales: { //hides the y axis
+      yAxes: [{
+        display: false,
+        ticks: {
+          display: true,
+          min: 0
         }
-      },
-      watch: {
-        chartData(){
-          this.renderChart(this.chartData)
+      }],
+      xAxes: [{
+        ticks: {
+          display: false, //this will remove only the label from tick
+          //callback: function(value, index, values) {      // with that we could hide the x-ticks when 4
+          //if (index <4) return value
+          //}
+          min: 0
+        },
+        scaleLabel: {
+          display: false,
+          labelString: 'Suchergebnisse'
         }
-      },
-      mounted () {
-        // Overwriting base render method with actual data.
-        this.renderChart(this.chartData)
-      },
+      }]
     }
+  }
+
+
+
+  import {Pie} from 'vue-chartjs'
+
+  export default {
+    extends: Pie,
+    props: {
+      chartData: {
+        type: Object //TODO add some validation
+      }
+    },
+    watch: {
+      chartData() {
+        this.renderChart(this.chartData, options)
+      }
+    },
+    mounted() {
+      // Overwriting base render method with actual data.
+      this.renderChart(this.chartData, options)
+    },
+  }
 </script>
 
