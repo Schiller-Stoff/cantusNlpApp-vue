629200b930b70449195230cac996f9d92e692155
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-31 10:49:57 +0200

    now using cantus_dummy_data

--- cantusNlp_vueApp/src/data/defaultWordsWordcloud.js
+++ cantusNlp_vueApp/src/data/defaultWordsWordcloud.js
@@ -1,41 +1,130 @@
 export const defaultWords = [
   {
-  "name": "Cat",
-  "value": 26
+    'name': 'quidam',
+    'value': 122
   },
   {
-    "name": "fish",
-    "value": 19
+    'name': 'bonus',
+    'value': 141
   },
   {
-    "name": "things",
-    "value": 18
-  },
-  {
-    "name": "look",
-    "value": 16
-  },
-  {
-    "name": "two",
-    "value": 15
-  },
-  {
-    "name": "fun",
-    "value": 9
-  },
-  {
-    "name": "know",
-    "value": 9
-  },
-  {
-    "name": "good",
-    "value": 9
-  },
-  {
-    "name": "play",
-    "value": 6
-  }
-]
+    'name': 'terra',
+    'value': 174
+  }, {'name': 'eo1', 'value': 244}, {'name': 'iesu', 'value': 107}, {'name': 'unus', 'value': 209}, {
+    'name': 'laudo',
+    'value': 390
+  }, {'name': 'audio', 'value': 150}, {'name': 'gratia', 'value': 111}, {'name': 'nox', 'value': 138}, {
+    'name': 'pater',
+    'value': 236
+  }, {'name': 'lectio', 'value': 290}, {'name': 'propter', 'value': 124}, {
+    'name': 'christi',
+    'value': 128
+  }, {'name': 'alleluia', 'value': 342}, {'name': 'do', 'value': 168}, {'name': 'magnus', 'value': 237}, {
+    'name': 'f',
+    'value': 193
+  }, {'name': 'confiteor', 'value': 115}, {'name': 'quaesumus', 'value': 159}, {
+    'name': 'b',
+    'value': 220
+  }, {'name': 'evangelium', 'value': 142}, {'name': 'hic', 'value': 787}, {
+    'name': 'beatus',
+    'value': 508
+  }, {'name': 'dies', 'value': 382}, {'name': 'nomen', 'value': 110}, {'name': 'd', 'value': 108}, {
+    'name': 'sabbatum',
+    'value': 119
+  }, {'name': 'oro', 'value': 134}, {'name': 'tuus', 'value': 438}, {'name': 'finio', 'value': 119}, {
+    'name': 'sancio',
+    'value': 1108
+  }, {'name': 'debeo', 'value': 211}, {'name': 'tantus', 'value': 144}, {'name': 'puer', 'value': 129}, {
+    'name': 'homo',
+    'value': 179
+  }, {'name': 'mitto', 'value': 390}, {'name': 'canto', 'value': 285}, {
+    'name': 'verbum',
+    'value': 127
+  }, {'name': 'tempus', 'value': 146}, {'name': 'duo', 'value': 145}, {'name': 'pascha', 'value': 117}, {
+    'name': '?',
+    'value': 150
+  }, {'name': 'nocturnus', 'value': 277}, {'name': 'vespera', 'value': 121}, {
+    'name': 'c',
+    'value': 256
+  }, {'name': 'corpus', 'value': 165}, {'name': 'advenio', 'value': 113}, {
+    'name': 'filius',
+    'value': 159
+  }, {'name': 'sui', 'value': 155}, {'name': 'dius', 'value': 230}, {'name': 'idem', 'value': 205}, {
+    'name': 'sacerdos1',
+    'value': 175
+  }, {'name': 'ab', 'value': 1915}, {'name': 'alius2', 'value': 197}, {
+    'name': 'christus',
+    'value': 101
+  }, {'name': 'spiritus', 'value': 179}, {'name': 'credo', 'value': 117}, {
+    'name': 'cano',
+    'value': 131
+  }, {'name': 'angelus', 'value': 173}, {'name': 'psalmus', 'value': 187}, {
+    'name': 'unde',
+    'value': 113
+  }, {'name': 'celebro', 'value': 150}, {'name': 'ideo', 'value': 217}, {
+    'name': 'festus1',
+    'value': 293
+  }, {'name': 'dominus', 'value': 2513}, {'name': 'sicut', 'value': 252}, {
+    'name': 'totus1',
+    'value': 139
+  }, {'name': 'usque', 'value': 252}, {'name': 'feria', 'value': 237}, {'name': 'ille', 'value': 339}, {
+    'name': 'iste',
+    'value': 184
+  }, {'name': 'ipse', 'value': 241}, {'name': 'sextus1', 'value': 174}, {
+    'name': 'nos',
+    'value': 226
+  }, {'name': 'jejunium', 'value': 145}, {'name': 'tu', 'value': 727}, {'name': 'crux', 'value': 195}, {
+    'name': 'vir',
+    'value': 166
+  }, {'name': 'meo', 'value': 134}, {'name': 'glorior', 'value': 259}, {
+    'name': 'verro',
+    'value': 102
+  }, {'name': 'populus1', 'value': 168}, {'name': 'video', 'value': 320}, {
+    'name': 'deus',
+    'value': 1276
+  }, {'name': 'pono', 'value': 138}, {'name': 'apostolus', 'value': 179}, {'name': 'scio', 'value': 109}, {
+    'name': 'fio',
+    'value': 174
+  }, {'name': 'rex', 'value': 130}, {'name': 'tres', 'value': 213}, {'name': 'ecclesia', 'value': 197}, {
+    'name': 'is',
+    'value': 629
+  }, {'name': 'primus', 'value': 375}, {'name': 'quartum', 'value': 120}, {
+    'name': 'virtus',
+    'value': 111
+  }, {'name': 'omnipotens', 'value': 149}, {'name': 'venio', 'value': 414}, {
+    'name': 'hora1',
+    'value': 118
+  }, {'name': 'qui1', 'value': 940}, {'name': 'verus', 'value': 271}, {'name': 'habeo', 'value': 242}, {
+    'name': 'dico2',
+    'value': 1126
+  }, {'name': 'lego2', 'value': 313}, {'name': 'omnes', 'value': 231}, {'name': 'justus', 'value': 288}, {
+    'name': 'vel',
+    'value': 220
+  }, {'name': 'sequor', 'value': 190}, {'name': 'edo1', 'value': 159}, {'name': 'suo', 'value': 162}, {
+    'name': 'anima',
+    'value': 104
+  }, {'name': 'ecce', 'value': 193}, {'name': 'benedico', 'value': 277}, {
+    'name': 'iesus',
+    'value': 140
+  }, {'name': 'virgo', 'value': 160}, {'name': 'omne', 'value': 404}, {
+    'name': 'colligo1',
+    'value': 112
+  }, {'name': 'noster', 'value': 366}, {'name': 'dominicus', 'value': 409}, {
+    'name': 'ago',
+    'value': 155
+  }, {'name': 'sum1', 'value': 987}, {'name': 'officium', 'value': 243}, {
+    'name': 'caelum1',
+    'value': 187
+  }, {'name': 'tertius', 'value': 294}, {'name': 'ego', 'value': 438}, {
+    'name': 'nativitas',
+    'value': 116
+  }, {'name': 'secundus1', 'value': 388}, {'name': 'mare', 'value': 187}, {
+    'name': 'oratio',
+    'value': 306
+  }, {'name': 'facio', 'value': 393}, {'name': 'possum', 'value': 132}, {'name': 'meus', 'value': 331}, {
+    'name': 'morior',
+    'value': 121
+  }]
 
 export const defaultColors = [
   '#1f77b4', '#629fc9', '#94bedb', '#c9e0ef'
