1116bedcf6d629325511afe26f88561be2f5f852
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-05-31 10:00:28 +0200

    created own js for exporting default-dummy ata for the wordcloud

---
+++ cantusNlp_vueApp/src/data/defaultWordsWordcloud.js
@@ -0,0 +1,42 @@
+export const defaultWords = [
+  {
+  "name": "Cat",
+  "value": 26
+  },
+  {
+    "name": "fish",
+    "value": 19
+  },
+  {
+    "name": "things",
+    "value": 18
+  },
+  {
+    "name": "look",
+    "value": 16
+  },
+  {
+    "name": "two",
+    "value": 15
+  },
+  {
+    "name": "fun",
+    "value": 9
+  },
+  {
+    "name": "know",
+    "value": 9
+  },
+  {
+    "name": "good",
+    "value": 9
+  },
+  {
+    "name": "play",
+    "value": 6
+  }
+]
+
+export const defaultColors = [
+  '#1f77b4', '#629fc9', '#94bedb', '#c9e0ef'
+]
