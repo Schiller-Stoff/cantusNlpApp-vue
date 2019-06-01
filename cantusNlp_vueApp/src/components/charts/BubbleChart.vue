b61a9b4482bae9f858e0d6af0bf3a47250d311ee
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-06-01 10:13:46 +0200

    added charts correctly to this repo

--- cantusNlp_vueApp/src/components/charts/BubbleChart.vue
+++ cantusNlp_vueApp/src/components/charts/BubbleChart.vue
@@ -3,6 +3,39 @@
   // CommitChart.js
   import {Bubble} from 'vue-chartjs'
 
+  let options = {
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    tooltips: {
+      //for displaying tooltips
+    },
+    scales: { //hides the y axis
+      yAxes: [{
+        display: true,
+        ticks: {
+          display: true
+          //min: 0
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          display: false //this will remove only the label from tick
+          //callback: function(value, index, values) {      // with that we could hide the x-ticks when 4
+          //if (index <4) return value
+          //}
+          //min: 0
+        },
+        scaleLabel: {
+          display: false,
+          labelString: 'Suchergebnisse'
+        }
+      }]
+    }
+  }
+
   export default {
     extends: Bubble,
     props: {
@@ -12,12 +45,12 @@
     },
     watch: {
       datasets(){
-        this.renderChart(this.chartData)
+        this.renderChart(this.chartData, options)
       }
     },
     mounted() {
       // Overwriting base render method with actual data.
-      this.renderChart(this.chartData)
+      this.renderChart(this.chartData, options)
     }
   }
 </script>
